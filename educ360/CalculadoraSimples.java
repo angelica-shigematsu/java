3cd4d7eda2147b91d3d5b96b23e4329810fcc079
Angélica Shigematsu <78798754+angelica-shigematsu@users.noreply.github.com>
2023-02-06 16:00:49 -0300

    add exception of error input

--- educ360/CalculadoraSimples.java
+++ educ360/CalculadoraSimples.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalculadoraSimples {
@@ -5,7 +6,7 @@ public class CalculadoraSimples {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		int value1 = 0, value2 = 0, result = 0;
+		double value1 = 0, value2 = 0, result = 0;
 
 		String operatorName = "";
 		char operator;
@@ -17,41 +18,46 @@ public class CalculadoraSimples {
 		boolean exitsError = false;
 		int divideByZero = 0;
 		String messageError = "";
-
-		System.out.println("Digite o 1º valor: ");
-		value1 = scan.nextInt();
-
-		System.out.println("Digite o 2º valor: ");
-		value2 = scan.nextInt();
-
-		System.out.println("Digite o operador (+,-,*,/) ");
-		operator = scan.next().charAt(0);
-
-		if (operator == sum) {
-			result = value1 + value2;
-			operatorName = "soma";
-		} else if (operator == subtract) {
-			result = value1 - value2;
-			operatorName = "subtração";
-		} else if (operator == multiply) {
-			result = value1 * value2;
-			operatorName = "multiplicação";
-		}else if (operator == divide && value2 != divideByZero) {
-			result = value1 / value2;
-			operatorName = "divisão";
-		}else if(value2 == divideByZero){
-			exitsError = true;
-			messageError = "não pode dividir por zero";
-		}else{
-			exitsError = true;
-			messageError = "não existe esse operador";
-		}
 		
-		
-		if(exitsError) {
-			System.out.println("Erro: " + messageError);
-		}else {
-			System.out.println("Value of " + operatorName + " é : " + result);
+		try {
+
+			System.out.println("Digite o 1º valor: ");
+			value1 = scan.nextDouble();
+	
+			System.out.println("Digite o 2º valor: ");
+			value2 = scan.nextDouble();
+	
+			System.out.println("Digite o operador (+,-,*,/) ");
+			operator = scan.next().charAt(0);
+	
+			if (operator == sum) {
+				result = value1 + value2;
+				operatorName = "soma";
+			} else if (operator == subtract) {
+				result = value1 - value2;
+				operatorName = "subtração";
+			} else if (operator == multiply) {
+				result = value1 * value2;
+				operatorName = "multiplicação";
+			}else if (operator == divide && value2 != divideByZero) {
+				result = value1 / value2;
+				operatorName = "divisão";
+			}else if(value2 == divideByZero){
+				exitsError = true;
+				messageError = "não pode dividir por zero";
+			}else{
+				exitsError = true;
+				messageError = "não existe esse operador";
+			}
+			
+			
+			if(exitsError) {
+				System.out.println("Erro: " + messageError);
+			}else {
+				System.out.println("Valor " + operatorName + " é : " + result);
+			}
+		}catch(InputMismatchException e) {
+			System.out.println("Entrada de tipo de dados errado");
 		}
 		scan.close();
 	}
