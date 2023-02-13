c48303f12bcf9154a960e20abb7ac9c27e9a8dfc
Angélica Shigematsu <78798754+angelica-shigematsu@users.noreply.github.com>
2023-02-13 18:45:12 -0300

    fix calculadora

--- educ360/CalculadoraPOO/src/br/com/educ/calculadora/Calculadora.java
+++ educ360/CalculadoraPOO/src/br/com/educ/calculadora/Calculadora.java
@@ -2,54 +2,21 @@ package br.com.educ.calculadora;
 
 public class Calculadora {
 
-	private double value1;
-	private double value2;
-	private double operator;
-	
-	public Calculadora(double value1, double value2, char operator) {
-		this.value1 = value1;
-		this.value2 = value2;
-		this.operator = operator;
-	}
-	
-	public double getValue1() {
-		return value1;
-	}
-
-	public void setValue1(double value1) {
-		this.value1 = value1;
-	}
-
-	public double getValue2() {
-		return value2;
-	}
-
-	public void setValue2(double value2) {
-		this.value2 = value2;
-	}
-
-	public double getOperator() {
-		return operator;
-	}
-
-	public void setOperator(double operator) {
-		this.operator = operator;
-	}
-
 	public double calcular(double value1, double value2, char operator) {
 		switch(operator) {
-		case '+': 
-			return sum(value1, value2);
-		case '-': 
-			return subtract(value1, value2);
-		case '*': 
-			return multiply(value1, value2);
-		case '/': 
-			return divide(value1, value2);
-		default: 
-			return 0;
+			case '+': 
+				return sum(value1, value2);
+			case '-': 
+				return subtract(value1, value2);
+			case '*': 
+				return multiply(value1, value2);
+			case '/': 
+				return divide(value1, value2);
+			case '^': 
+				return potentiation(value1, value2);
+			default: 
+				return 0;
 		}
-
 	}
 
 	private double sum(double value1, double value2) {
@@ -65,7 +32,14 @@ public class Calculadora {
 	}
 
 	private double divide(double value1, double value2) {
-		return value1 / value2;
+		if (value2 != 0)
+			return value1 / value2;
+		else
+			return 0;
+	}
+	private double potentiation(double value1, double value2) {
+		return Math.pow(value1, value2);
+		
 	}
 
 }
--- educ360/CalculadoraPOO/src/br/com/educ/calculadora/Main.java
+++ educ360/CalculadoraPOO/src/br/com/educ/calculadora/Main.java
@@ -5,25 +5,32 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		double num1 = 0, num2 = 0;
 		char operator;
-		
+		char attempts = 'n';
+
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Digite 1 º número: ");
-		num1 = scan.nextDouble();
-		
-		System.out.println("Digite 2 º número: ");
-		num2 = scan.nextDouble();
-		
-		System.out.println("Digite o operador ");
-		operator = scan.next().charAt(0);
-		
-		Calculadora cal = new Calculadora(num1, num2, operator);
-		
-		System.out.println(cal.calcular(num1, num2, operator));
-		
+		do {
+			System.out.println("Digite 1 º número: ");
+			num1 = scan.nextDouble();
+
+			System.out.println("Digite 2 º número: ");
+			num2 = scan.nextDouble();
+
+			System.out.println("Digite o operador ");
+			operator = scan.next().charAt(0);
+
+			Calculadora cal = new Calculadora();
+
+			System.out.println(cal.calcular(num1, num2, operator));
+			
+			System.out.println("Deseja continuar (s/n): ");
+			attempts = scan.next().charAt(0);
+			
+		}while(attempts == 's' || attempts == 'S');
+		scan.close();
 
 	}
 
