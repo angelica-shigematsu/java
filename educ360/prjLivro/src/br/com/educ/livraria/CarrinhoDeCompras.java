88ead59bac9757177a55671fc6f4ad6a9b7cf44e
Angélica Shigematsu <78798754+angelica-shigematsu@users.noreply.github.com>
2023-03-04 16:49:30 -0300

    add discount count

--- educ360/prjLivro/src/br/com/educ/livraria/CarrinhoDeCompras.java
+++ educ360/prjLivro/src/br/com/educ/livraria/CarrinhoDeCompras.java
@@ -1,7 +1,9 @@
 package br.com.educ.livraria;
 
+
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class CarrinhoDeCompras {
 	int sequencia = 0;
@@ -19,7 +21,7 @@ public class CarrinhoDeCompras {
 		System.out.println("--------Ebook---------");
 		ArrayList<Livro> ordemTitulo = new ArrayList<Livro>(listaDeLivros);
 		
-		//Collections.sort(ordemTitulo);
+		Collections.sort(ordemTitulo, Comparator.comparing(Livro::getTitulo));
 		
 		for (Livro ebook  : listaDeLivros) {
 			if (ebook instanceof Ebook) {
@@ -36,6 +38,7 @@ public class CarrinhoDeCompras {
 			}
 		}	
 	}
+	
 	public double calcularPrecoTotal() {
 		double preco = 0;
 		
@@ -44,4 +47,18 @@ public class CarrinhoDeCompras {
 		}
 		return preco;
 	}
+	
+	public double calcularPrecoTotal(double desconto) {
+		double preco = 0;
+		
+		System.out.println(desconto);
+		for(int index = 0; index < listaDeLivros.size(); index++) {
+			preco += listaDeLivros.get(index).calcularPrecoTotal();
+		}
+		preco = preco - (desconto/100.0);
+	
+		return preco;
+	}
+	
+	
 }
