4504c68b1bc977f368cb4e23789c581288a3abf0
Angélica Shigematsu <78798754+angelica-shigematsu@users.noreply.github.com>
2023-05-17 16:29:06 -0300

    feat customer
    create, read, update, delete customer in SpringBoot

---
+++ educ360/banco-digital-springboot/banco-digital/src/test/java/br/com/cdb/bancoDigital/BancoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cdb.bancoDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
